e7a7f8db15baee7463e18fe8b03bbc5daa36c2dc
tepn <pioupioutte@gmail.com>
2019-07-08 18:04:21 +0200

    dummy buttons for attributes using Bootstrap
    Used Bootstrap to have a visual for the buttons on the attributes.
    Made a POC for the "Force" attribute
    
    Signed-off-by: tepn <pioupioutte@gmail.com>

--- scripts/characterSheet.js
+++ scripts/characterSheet.js
@@ -6,4 +6,18 @@ $(function(){
 		$("#left-pc").val(20 - $(this).val());
 		$("#attribute-points").val(38 + 2 * $(this).val());
 	});
+	$("#btn-d-for").on('click', function(){
+		console.log($("#attr-FOR").val());
+		if ($("#attr-FOR").val() > 7) {
+			$("#attr-FOR").val(parseInt($("#attr-FOR").val()) - 1)
+			$("#attribute-points").val(parseInt($("#attribute-points").val()) + 1);
+		}
+	});
+	$("#btn-u-for").on('click', function(){
+		console.log($("#attr-FOR").val());
+		if ($("#attr-FOR").val() < 20) {
+			$("#attr-FOR").val(parseInt($("#attr-FOR").val()) + 1)
+			$("#attribute-points").val(parseInt($("#attribute-points").val()) - 1);
+		}
+	});
 });

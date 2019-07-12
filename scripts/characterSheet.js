a34bcd8cab8cf898e31727ca45e385ba0bde0ce4
tepn <pioupioutte@gmail.com>
2019-07-12 13:23:01 +0200

    lay out character sheet and attributes fields are functionals
    remove some console logs
    all attributes buttons are functional
    javascript modifications.
    TODO: review the lookup tables as values are most likely wrong

--- scripts/characterSheet.js
+++ scripts/characterSheet.js
@@ -1,24 +1,33 @@
+const tables = {
+	attributes: [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 9, 12, 16, 20, 25, 31, 38],
+	skills: [0, 1, 2, 3, 4, 5, 7, 9, 11, 13, 15, 18, 23, 29, 35, 41, 47, 53, 59, 65]
+};
+
+function refreshRemainingAp() {
+	var values = $(".attr").map(function(){return $(this).val();}).get();
+	var sum = 0;
+	for (i = 0; i < values.length; i++) {
+		sum += tables["attributes"][parseInt(values[i]) - 1];
+	}
+	var attPts = 38 + $("#pc-to-pa").val() * 2;
+	$("#attribute-points").val(attPts - sum);
+}
+
 $(function(){
 	console.log(comps);
-	$(".used-pc").change(function(){
+	$("#pc-to-pa").change(function(){
 		$("#left-pc").val(20 - $(this).val());
+		$("#attribute-points").val($("#attribute-points").val() + 2 * $(this).val());
+		refreshRemainingAp();
 	});
-	$(".pc-to-pa").change(function(){
-		$("#left-pc").val(20 - $(this).val());
-		$("#attribute-points").val(38 + 2 * $(this).val());
+	$(".attr").change(function(){
+		refreshRemainingAp();
 	});
-	$("#btn-d-for").on('click', function(){
-		console.log($("#attr-FOR").val());
-		if ($("#attr-FOR").val() > 7) {
-			$("#attr-FOR").val(parseInt($("#attr-FOR").val()) - 1)
-			$("#attribute-points").val(parseInt($("#attribute-points").val()) + 1);
-		}
-	});
-	$("#btn-u-for").on('click', function(){
-		console.log($("#attr-FOR").val());
-		if ($("#attr-FOR").val() < 20) {
-			$("#attr-FOR").val(parseInt($("#attr-FOR").val()) + 1)
-			$("#attribute-points").val(parseInt($("#attribute-points").val()) - 1);
+	$(":input[type=number]").on("change", function(){
+		if (($(this).val() < $(this).attr("min")) || ($(this).val() > $(this).attr("max"))) {
+			$(this).addClass("is-invalid");
+		} else {
+			$(this).removeClass("is-invalid");
 		}
 	});
 });

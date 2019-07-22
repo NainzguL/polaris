48f3632fc394b5e476b5eb43e034c9a19974ce57
NainzguL <nainzgul@gmail.com>
2019-07-22 14:18:56 +0200

    Add competences' dynamic gestion + i18n + few code modifications
    The competences are now brought from the json file. Once read, they're
    published into the table dynamically.
    The category of each competences are set as a "title" of each section
    (could be cut in different tables afterward, and used to show/hide the
    whole section).
    The requirements are now set so we can then add color to competences
    that doesn't have the proper requirements.
    Also added i18n, in order to manage only keywords, and then benefit of
    the i18n file to recover the names.
    Changes some typo/misswriten elements.
    Added some CSS style.
    Change most scope of variables (var to let)

--- scripts/characterSheet.js
+++ scripts/characterSheet.js
@@ -5,41 +5,49 @@ const tables = {
 };
 
 function refreshRemainingAp() {
-	var values = $(".attr").map(function(){return $(this).val();}).get();
-	var sum = 0;
+	let values = $(".attr").map(function(){return $(this).val();}).get();
+	let sum = 0;
 	for (i = 0; i < values.length; i++) {
 		sum += tables["attributes"][parseInt(values[i]) - 1];
 	}
-	var attPts = 38 + $("#pc-to-pa").val() * 2;
+	let attPts = 38 + $("#pc-to-pa").val() * 2;
 	$("#attribute-points").val(attPts - sum);
 }
 
 function refreshOneCompetence(cmp) {
-	console.log(cmp);
 	var cmpId = cmp.attr('id').split("-")[0];
 	var baseVal = parseInt($("#".concat(cmpId).concat("-base-field")).val());
 	var maitVal = parseInt($("#".concat(cmpId).concat("-mastery-field")).val());
-	console.log(baseVal);
-	console.log(maitVal);
 	cmp.val(baseVal + maitVal);
 }
 
 function refreshCompetence() {
 	$(".cmp-base").map(function(){
-		var associatedAttr = comps[$(this).attr('id').split("-")[0]]["attr"];
-		var sum = 0;
-		for (i = 0; i < associatedAttr.length; i++) {
-			var attrFieldId = "#".concat(associatedAttr[i]).concat("-field");
-			var attrVal = parseInt($(attrFieldId).val());
-			sum += tables["attributesToBase"][attrVal];
+		let comp = undefined;
+		let compName = $(this).attr('id').split("-")[0];
+		for(let categoryName in comps){
+			let category = comps[categoryName];
+			comp = category[compName];
+			if(comp){
+				break;
+			}
 		}
-		$(this).val(sum);
+		if(comp){
+			let associatedAttr = comp["attr"];
+			let sum = 0;
+			for (i = 0; i < associatedAttr.length; i++) {
+				let attrVal = parseInt($("#" + associatedAttr[i] + "-field").val());
+				sum += tables["attributesToBase"][attrVal];
+			}
+			$(this).val(sum);
+		}
 	});
-	$(".cmp-total").map(function(){ refreshOneCompetence($(this));});
+	$(".cmp-total").map(function(){ refreshOneCompetence($(this)); });
 }
 
 $(function(){
 	console.log(comps);
+	console.log(locale);
 	$("#pc-to-pa").change(function(){
 		$("#left-pc").val(20 - $(this).val());
 		$("#attribute-points").val($("#attribute-points").val() + 2 * $(this).val());
@@ -50,17 +58,30 @@ $(function(){
 		refreshCompetence();
 	});
 	$(":input[type=number]").on("change", function(){
-		const min = parseInt($(this).attr("min"));
-		const max = parseInt($(this).attr("max"));
-		const n = parseInt($(this).val());
+		let min = parseInt($(this).attr("min"));
+		let max = parseInt($(this).attr("max"));
+		let n = parseInt($(this).val());
 		if ((n < min) || (n > max)) {
 			$(this).addClass("is-invalid");
 		} else {
 			$(this).removeClass("is-invalid");
 		}
 	});
+	for(let categoryName in comps){
+		let category = comps[categoryName];
+		$("#compTable").append('<tr class="table-primary"><td colspan="5" class="compHeader">' + locale[categoryName] + '</td></tr>');
+		for(let compName in category){
+			let compRow = document.createElement("tr");
+			$(compRow).append('<td>' + locale[compName] + (category[compName].requirement ? ' †' : '') + '</td>')
+			$(compRow).append('<td class="compAttr">' + category[compName].attr[0] + '/' + category[compName].attr[1] + '</td>')
+			$(compRow).append('<td><input type="number" value="0" min="0" readonly="readonly" class="form-control cmp-base" id="' + compName + '-base-field" /></td>')
+			$(compRow).append('<td><input type="number" value="' + category[compName].base + '" min="' + category[compName].base + '" max="20" class="form-control cmp-mastery" id="' + compName + '-mastery-field" /></td>')
+			$(compRow).append('<td><input type="number" value="0" readonly="readonly" class="form-control cmp-total" id="' + compName + '-total-field" /></td>')
+			$("#compTable").append(compRow);
+		}
+	}
 	$(".cmp-mastery").on("change", function(){
-		var cmpId = $(this).attr('id').split("-")[0];
+		let cmpId = $(this).attr('id').split("-")[0];
 		refreshOneCompetence($("#".concat(cmpId).concat("-total-field")));
 	});
 });

b49fcc300b1e179db314c6141edc3927c60a082a
NainzguL <nainzgul@gmail.com>
2019-07-26 14:48:46 +0200

    Add prerequisite + specificity to competences
    Competences are now computed with their specificty (limiting and natural
    progression), and also they are highlight when their prerequisite is not
    fulfilled.
    Corrected some typo (trailing spaces, inverted letters), and added some
    scope operator to the loop indexes when needed (causes some
    crashs/recursive troubles).

--- scripts/characterSheet.js
+++ scripts/characterSheet.js
@@ -1,13 +1,12 @@
 const tables = {
-	attributes: [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 9, 12, 16, 20, 25, 31, 38],
-	skills: [0, 1, 2, 3, 4, 5, 7, 9, 11, 13, 15, 18, 23, 29, 35, 41, 47, 53, 59, 65],
-	attributesToBase: [-1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5],
+	attributes: [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 17, 20],
+	attributesToBase: [-4, -4, -4, -4, -3, -2, -1, -1, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6],
 };
 
 function refreshRemainingAp() {
 	let values = $(".attr").map(function(){return $(this).val();}).get();
 	let sum = 0;
-	for (i = 0; i < values.length; i++) {
+	for (let i=0; i<values.length; i++) {
 		sum += tables["attributes"][parseInt(values[i]) - 1];
 	}
 	let attPts = 38 + $("#pc-to-pa").val() * 2;
@@ -15,10 +14,62 @@ function refreshRemainingAp() {
 }
 
 function refreshOneCompetence(cmp) {
-	var cmpId = cmp.attr('id').split("-")[0];
-	var baseVal = parseInt($("#".concat(cmpId).concat("-base-field")).val());
-	var maitVal = parseInt($("#".concat(cmpId).concat("-mastery-field")).val());
-	cmp.val(baseVal + maitVal);
+	let comp = undefined;
+	let compId = cmp.attr('id').split("-")[0];
+	let dependantComps = [];
+	for(let categoryName in comps){
+		let category = comps[categoryName];
+		if(category[compId] !== undefined){
+			comp = category[compId];
+		}
+
+		for(let compName in comps[categoryName]){
+			if(comps[categoryName][compName].requirement !== undefined && comps[categoryName][compName].requirement[compId] !== undefined){
+				let compField = $('#' + compName + '-total-field');
+				if(compField.length > 0){
+					dependantComps.push(compField);
+				}
+			}
+		}
+	}
+
+	if(comp){
+		//Base
+		let associatedAttr = comp["attr"];
+		let baseVal = 0;
+		for (let i=0; i<associatedAttr.length; i++) {
+			let attrVal = parseInt($("#" + associatedAttr[i] + "-field").val());
+			baseVal += tables["attributesToBase"][attrVal];
+		}
+		$("#".concat(compId).concat("-base-field")).val(baseVal);
+
+		//Mastery
+		let mastVal = parseInt($("#".concat(compId).concat("-mastery-field")).val());
+
+		//Total
+		cmp.val(baseVal + mastVal);
+		if(comp.requirement){
+			let fulfillRequirements = true;
+			for(let requirement in comp.requirement){
+				let requiredLevel = $('#' + requirement + '-total-field').val();
+				if(requiredLevel === undefined || requiredLevel < comp.requirement[requirement]){
+					$(cmp).parents('tr').addClass('table-danger');
+					fulfillRequirements = false;
+					break;
+				}
+			}
+			if(fulfillRequirements){
+				$(cmp).parents('tr').removeClass('table-danger');
+			}
+		}
+		
+		if(dependantComps.length > 0){
+			for (let i=0; i<dependantComps.length; i++) {
+				//Recursive call, be careful with your comp list !
+				refreshOneCompetence(dependantComps[i]);
+			}
+		}
+	}
 }
 
 /**
@@ -29,20 +80,20 @@ function refreshOneCompetence(cmp) {
  * @param careerCmp an bool, false if the competence is not the carrer, in this case the mastery level cost double.
  * 
  */
-function getCmpPointCostOneLevel(curentMastery, careerCmp) {
-	let ret =0
-	if (curentMastery <= 5) {
-		ret = 1;
+function getCmpPointCostOneLevel(currentMastery, careerCmp) {
+	let ret = 1;
+	if (currentMastery > 5) {
+		ret++;
+		if (currentMastery > 10) {
+			ret++;
+			let threshold = currentMastery - 11;
+			while(threshold > 0){
+				ret +=2;
+				threshold--;
+			}
+		}
 	}
-	else if (curentMastery <= 10) {
-		ret = 2
-	}
-	else if (curentMastery <= 15) {
-		ret = 3
-	}
-	else {
-		ret = 3 + curentMastery - 15;
-	}
+
 	return careerCmp ? ret : ret*2;
 }
 
@@ -58,7 +109,7 @@ function getCmpPointCost(curentMastery, wantedMastery, careerCmp) {
 	for (let i = curentMastery + 1; i <= wantedMastery; i++) {
 		total = total + getCmpPointCostOneLevel(i, careerCmp);
 	}
-	return total
+	return total;
 }
 
 function refreshCmpPoint() {
@@ -75,33 +126,18 @@ function refreshLeftPc() {
 	$("#left-pc").val(20 - pcForPa - pcForCmp);
 }
 
-function refreshCompetence() {
-	$(".cmp-base").map(function(){
-		let comp = undefined;
-		let compName = $(this).attr('id').split("-")[0];
-		for(let categoryName in comps){
-			let category = comps[categoryName];
-			comp = category[compName];
-			if(comp){
-				break;
-			}
-		}
-		if(comp){
-			let associatedAttr = comp["attr"];
-			let sum = 0;
-			for (i = 0; i < associatedAttr.length; i++) {
-				let attrVal = parseInt($("#" + associatedAttr[i] + "-field").val());
-				sum += tables["attributesToBase"][attrVal];
-			}
-			$(this).val(sum);
-		}
-	});
+function refreshCompetences() {
 	$(".cmp-total").map(function(){ refreshOneCompetence($(this)); });
 }
 
+function translate(value){
+	if(locale[value]){
+		return locale[value]; 
+	}
+	return value;
+}
+
 $(function(){
-	console.log(comps);
-	console.log(locale);
 	$("#pc-to-pa").change(function(){
 		$("#attribute-points").val($("#attribute-points").val() + 2 * $(this).val());
 		refreshLeftPc();
@@ -113,9 +149,14 @@ $(function(){
 	});
 	$(".attr").change(function(){
 		refreshRemainingAp();
-		refreshCompetence();
+		refreshCompetences();
 		refreshCmpPoint();
+		
+		let attrId = $(this).attr('id').split('-')[0];
+		$('#' + attrId + '-apt-nat').val(tables["attributesToBase"][$(this).val()]);
 	});
+	
+	//This one seems useless
 	$(":input[type=number]").on("change", function(){
 		let min = parseInt($(this).attr("min"));
 		let max = parseInt($(this).attr("max"));
@@ -128,15 +169,43 @@ $(function(){
 	});
 	for(let categoryName in comps){
 		let category = comps[categoryName];
-		$("#compTable").append('<tr class="table-primary"><td colspan="5" class="compHeader">' + locale[categoryName] + '</td></tr>');
+		let categoryHidden = true;
 		for(let compName in category){
-			let compRow = document.createElement("tr");
-			$(compRow).append('<td>' + locale[compName] + (category[compName].requirement ? ' †' : '') + '</td>')
-			$(compRow).append('<td class="compAttr">' + category[compName].attr[0] + '/' + category[compName].attr[1] + '</td>')
-			$(compRow).append('<td><input type="number" value="0" min="0" readonly="readonly" class="form-control cmp-base" id="' + compName + '-base-field" /></td>')
-			$(compRow).append('<td><input type="number" value="' + category[compName].base + '" min="' + category[compName].base + '" max="20" class="form-control cmp-mastery" id="' + compName + '-mastery-field" /></td>')
-			$(compRow).append('<td><input type="number" value="0" readonly="readonly" class="form-control cmp-total" id="' + compName + '-total-field" /></td>')
-			$("#compTable").append(compRow);
+			//Make X comp invisible
+			if(category[compName].base !== null){
+				if(categoryHidden){
+					//First row of the category
+					$("#compTable").append('<tr class="table-primary"><td colspan="5" class="compHeader">' + locale[categoryName] + '</td></tr>');
+					categoryHidden = false;
+				}
+
+				let compRow = document.createElement("tr");
+				let specificity = '';
+				if(category[compName].requirement){
+					specificity+=' †';
+				}
+				if(category[compName].limiting){
+					specificity+=' •';
+				}
+				if(category[compName].naturalIncrease){
+					specificity+=' (PN)';
+				}
+
+				let baseValue = 0;
+				let sum = 0;
+				for (let i=0; i<category[compName].attr.length; i++) {
+					let attrVal = parseInt($("#" + category[compName].attr[i] + "-field").val());
+					sum += tables["attributesToBase"][attrVal];
+				}
+				$(this).val(sum);
+
+				$(compRow).append('<td>' + translate(compName) + specificity + '</td>')
+				$(compRow).append('<td class="compAttr">' + category[compName].attr[0] + '/' + category[compName].attr[1] + '</td>');
+				$(compRow).append('<td><input type="number" class="form-control cmp-base"    id="' + compName + '-base-field"    value="' + baseValue + '" readonly="readonly" /></td>');
+				$(compRow).append('<td><input type="number" class="form-control cmp-mastery" id="' + compName + '-mastery-field" value="' + category[compName].base + '" min="' + category[compName].base + '" /></td>');
+				$(compRow).append('<td><input type="number" class="form-control cmp-total"   id="' + compName + '-total-field"   value="' + (baseValue + category[compName].base) + '" readonly="readonly" /></td>');
+				$("#compTable").append(compRow);
+			}
 		}
 	}
 	$(".cmp-mastery").on("change", function(){
@@ -144,4 +213,6 @@ $(function(){
 		refreshOneCompetence($("#".concat(cmpId).concat("-total-field")));
 		refreshCmpPoint();
 	});
+
+	refreshCompetences();
 });
